71212449728a7f179267b2f0c1c630e279312f84
axvolkzki <amelia_abenoja@dlsu.edu.ph>
2024-11-20 15:14:19 +0800

    Added: Login and Signup fragments

--- app/src/main/java/com/mobdeve/s14/amelia_delacruz/bookcol/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s14/amelia_delacruz/bookcol/MainActivity.kt
@@ -7,27 +7,71 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mobdeve.s14.amelia_delacruz.bookcol.databinding.ActivityMainBinding
+import com.mobdeve.s14.amelia_delacruz.bookcol.fragments.LoginFragment
+import com.mobdeve.s14.amelia_delacruz.bookcol.fragments.SignupFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        val loginFragment = LoginFragment()
+        val signupFragment = SignupFragment()
+
+
+        // If user is not logged in, show the main activity layout
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction().apply {
+//                replace(viewBinding.flWrapper.id, loginFragment)
+//                commit()
+//            }
+//        }
+
+        // If user is logged in, show the base activity layout
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction().apply {
+//                replace(viewBinding.flWrapper.id, BaseActivity())
+//                commit()
+//            }
+//        }
+
+        // Temporary solution for navigation between login and signup fragments
+        viewBinding.btnLogIn.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                setCustomAnimations(
+                    R.anim.slide_in, // Enter animation
+                    R.anim.slide_out, // Exit animation
+                    R.anim.slide_in, // Pop enter animation
+                    R.anim.slide_out  // Pop exit animation
+                )
+
+
+
+                replace(viewBinding.flWrapper.id, loginFragment)
+                addToBackStack(null) // Allows navigating back to MainActivity
+                commit()
+            }
         }
 
-        // Find the button by its ID
-        val loginButton: Button = findViewById(R.id.buttonLogin)
+        viewBinding.btnSignUp.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                setCustomAnimations(
+                    R.anim.slide_in,
+                    R.anim.slide_out,
+                    R.anim.slide_in,
+                    R.anim.slide_out
+                )
 
-        // Set up the click listener for the login button
-        loginButton.setOnClickListener {
-            // Navigate to HomeActivity
-            val intent = Intent(this, BaseActivity::class.java)
-            startActivity(intent)
-            finish()
+
+                replace(viewBinding.flWrapper.id, signupFragment)
+                addToBackStack(null)
+                commit()
+            }
         }
     }
 }
