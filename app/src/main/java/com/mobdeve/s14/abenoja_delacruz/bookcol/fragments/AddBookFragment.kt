57c82064ec9a6d1472b06bf4d5bec4bb9dc388d2
axvolkzki <amelia_abenoja@dlsu.edu.ph>
2024-11-21 22:48:29 +0800

    Added: Camera feature to scan a barcode

--- app/src/main/java/com/mobdeve/s14/abenoja_delacruz/bookcol/fragments/AddBookFragment.kt
+++ app/src/main/java/com/mobdeve/s14/abenoja_delacruz/bookcol/fragments/AddBookFragment.kt
@@ -1,13 +1,19 @@
 package com.mobdeve.s14.abenoja_delacruz.bookcol.fragments
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.mlkit.vision.barcode.common.Barcode
 import com.mobdeve.s14.abenoja_delacruz.bookcol.R
+import com.mobdeve.s14.abenoja_delacruz.bookcol.activities.ISBNScannerActivity
 import com.mobdeve.s14.abenoja_delacruz.bookcol.databinding.FragmentAddBookBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -25,7 +31,35 @@ class AddBookFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    private lateinit var viewBinding : FragmentAddBookBinding
+    private val cameraPermission = android.Manifest.permission.CAMERA       // Camera permission
+    private lateinit var viewBinding : FragmentAddBookBinding               // ViewBinding
+
+    // Request permission launcher for camera
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+        if (isGranted) {
+            // Permission is granted
+            // Toast message for debugging
+            Toast.makeText(requireContext(), "Camera permission granted", Toast.LENGTH_SHORT).show()
+
+            // Start Scanner
+            startScanner()
+        } else {
+            // Permission is not granted
+            // Toast message for debugging
+            Toast.makeText(requireContext(), "Camera permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val scannerLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == AppCompatActivity.RESULT_OK) {
+                val barcodes = result.data?.getStringArrayListExtra("BARCODES")
+                barcodes?.let { handleScannedBarcodes(it) }
+            } else {
+                Toast.makeText(requireContext(), "No barcode scanned", Toast.LENGTH_SHORT).show()
+            }
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +74,8 @@ class AddBookFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_book, container, false)
+        viewBinding = FragmentAddBookBinding.inflate(inflater, container, false)
+        return viewBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -50,11 +85,7 @@ class AddBookFragment : Fragment() {
 
         this.viewBinding.llScanBarCode.setOnClickListener {
             if (checkCameraHardware(requireContext())) {
-                checkCameraPermission()
-                val camera = getCameraInstance()
-                if (camera != null) {
-                    camera.release()
-                }
+                requestCameraAndStartScanner()
             } else {
                 // Toast message for debugging
                 Toast.makeText(requireContext(), "No camera available", Toast.LENGTH_SHORT).show()
@@ -70,27 +101,67 @@ class AddBookFragment : Fragment() {
         }
     }
 
-    private fun checkCameraPermission() {
-        if (androidx.core.content.ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA) == android.content.pm.PackageManager.PERMISSION_GRANTED) {
-            // Permission is granted
-            // Toast message for debugging
-            Toast.makeText(requireContext(), "Camera permission granted", Toast.LENGTH_SHORT).show()
+
+    private fun requestCameraAndStartScanner() {
+        if (isPermissionGranted(cameraPermission)) {
+            startScanner()
         } else {
-            // Permission is not granted
-            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 1)
+            requestCameraPermission()
         }
     }
 
-    private fun getCameraInstance() : android.hardware.Camera? {
-        var c : android.hardware.Camera? = null
-        try {
-            c = android.hardware.Camera.open()
-        } catch (e : Exception) {
-            // Camera is not available (in use or does not exist)
-            // Toast message for debugging
-            Toast.makeText(requireContext(), "Camera is not available", Toast.LENGTH_SHORT).show()
+    private fun isPermissionGranted(cameraPermission: String): Boolean {
+        return requireContext().checkSelfPermission(cameraPermission) == android.content.pm.PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestCameraPermission() {
+        when {
+            shouldShowRequestPermissionRationale(cameraPermission) -> {
+                cameraPermissionRequest(
+                    positive = { openPermissionSetting() }
+                )
+                // Toast message for debugging
+                Toast.makeText(requireContext(), "Camera permission is needed to scan barcodes", Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                // Request permission
+                requestPermissionLauncher.launch(cameraPermission)
+            }
         }
-        return c
+    }
+
+    private fun openPermissionSetting() {
+        val intent = android.content.Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = android.net.Uri.fromParts("package", requireContext().packageName, null)
+        intent.data = uri
+        startActivity(intent)
+    }
+
+    private fun cameraPermissionRequest(positive: () -> Unit) {
+        android.app.AlertDialog.Builder(requireContext())
+            .setTitle("Camera Permission")
+            .setMessage("Camera permission is needed to scan barcodes")
+            .setPositiveButton("OK") { _, _ ->
+                positive()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+    }
+
+
+    private fun startScanner() {
+        val intent = Intent(requireContext(), ISBNScannerActivity::class.java)
+        scannerLauncher.launch(intent)
+    }
+
+    private fun handleScannedBarcodes(barcodes: List<String>) {
+        barcodes.forEach { barcode ->
+            // Update the UI or perform any action with the barcode
+            Toast.makeText(requireContext(), "Scanned: $barcode", Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
